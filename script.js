236ed9e997d93dfe99a2a1445b54def197636b6c
boo77 <beth.hammett@gmail.com>
2015-04-19 01:44:54 -0700

    jQuery
    Making the recipe interactive

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  console.log("Script included!");
+  $('img').click(function() {
+    $(this).fadeOut('slow');
+    });
+  $('ul li').click(function() {
+    $(this).css('color', 'red');
+    });
+  $('ol li').click(function() {
+     $('<p><em>Done! Next Step:</em></p>').appendTo(this);
+    });
+});
