61f223ddea7d0ffe99a0b6f63cf23395da8f10d8
boo77 <beth.hammett@gmail.com>
2015-04-21 00:29:57 -0700

    Update

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ $(document).ready(function() {
   $('img').click(function() {
     $(this).fadeOut('slow');
     });
-  $('ul li').click(function() {
-    $(this).css('color', 'red');
+  $('ul li').click(function() { 
+       $(this).css('color', 'red');
     });
   $('ol li').click(function() {
      $('<p><em>Done! Next Step:</em></p>').appendTo(this);
     });
 });
+
+
